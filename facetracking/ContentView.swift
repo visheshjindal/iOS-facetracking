b33c67f8f2d0222b38d92ebcce64fe439d87c98b
Vishesh Jindal <ece.vishesh@gmail.com>
2026-09-20 23:52:09 +0530

    Initial Commit

---
+++ facetracking/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  facetracking
+//
+//  Created by Vishesh Jindal on 20/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ facetracking/facetrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  facetrackingApp.swift
+//  facetracking
+//
+//  Created by Vishesh Jindal on 20/09/26.
+//
+
+import SwiftUI
+
+@main
+struct facetrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
